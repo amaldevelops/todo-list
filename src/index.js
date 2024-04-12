a6b6bb94b66dbf8e6b0661443507dfbc9aa81dfa
Amal K <12756149+1Amal@users.noreply.github.com>
2024-04-12 10:58:34 +1000

    Configured Webpack for HTML and CSS files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import "./index.css";
 console.log("JS Added");
+
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    element.innerHTML = "Hello Webpack";
+   element.classList.add('hello');
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
