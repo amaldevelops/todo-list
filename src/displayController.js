25ec4f25e4e2fc8aa13b7c7ba20f32400e5b832f
Amal K <12756149+1Amal@users.noreply.github.com>
2024-05-08 14:12:20 +1000

    Add loop to display LocalStorage data into the DOM

--- src/displayController.js
+++ src/displayController.js
@@ -77,52 +77,59 @@ export class displayController
     {
       const currentLocalStorage=initializeLocalStorage.localStorageAccessRead();
       console.log(currentLocalStorage);
+      console.log(currentLocalStorage["urgentAndImportant"].length);
       
       const taskListDiv=document.querySelector(".taskDisplay");
 
-      const taskDiv=document.createElement("div");
-      taskDiv.classList.add("individualTaskDetails");
-      taskListDiv.append(taskDiv);
+      for (let i=0;i<currentLocalStorage["urgentAndImportant"].length;i++)
+        {
+          const taskDiv=document.createElement("div");
+          taskDiv.classList.add("individualTaskDetails");
+          taskListDiv.append(taskDiv);
+    
+          const projectName=document.createElement("h2");
+          projectName.innerText=JSON.stringify(currentLocalStorage["urgentAndImportant"][i]);
+          taskDiv.append(projectName);
 
-      const projectName=document.createElement("h2");
-      projectName.innerText="Project Name: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["projectName"]);
-      taskDiv.append(projectName);
+          this.editButton=document.createElement("button");
+          this.editButton.classList.add("editButton");
+          this.editButton.innerText="Edit Task";
+          taskDiv.append(this.editButton);
+    
+          const deleteButton=document.createElement("button");
+          deleteButton.classList.add("deleteButton");
+          deleteButton.innerText="Delete Task";
+          taskDiv.append(deleteButton);
 
-      const taskName=document.createElement("p");
-      taskName.innerText="Task Title: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["taskTitle"]);
-      taskDiv.append(taskName);
+        }
 
-      const taskDetails=document.createElement("p");
-      taskDetails.innerText="Description: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["Description"]);
-      taskDiv.append(taskDetails);
-
-      const dueDate=document.createElement("p");
-      dueDate.innerText="Due Date: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["dueDate"]);
-      taskDiv.append(dueDate);
-
-      const priority=document.createElement("p");
-      priority.innerText="Priority: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["priority"]);
-      taskDiv.append(priority);
-
-      const editButton=document.createElement("button");
-      editButton.classList.add("editButton");
-      editButton.innerText="Edit Task";
-      taskDiv.append(editButton);
-
-      const deleteButton=document.createElement("button");
-      deleteButton.classList.add("deleteButton");
-      deleteButton.innerText="Delete Task";
-      taskDiv.append(deleteButton);
-
-      
+        document.querySelector(".editButton").addEventListener('click',()=>
+          {
+            console.log("Edit Button Clicked");
+          });
 
 
 
 
-      
+      // const taskName=document.createElement("p");
+      // taskName.innerText="Task Title: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["taskTitle"]);
+      // taskDiv.append(taskName);
+
+      // const taskDetails=document.createElement("p");
+      // taskDetails.innerText="Description: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["Description"]);
+      // taskDiv.append(taskDetails);
+
+      // const dueDate=document.createElement("p");
+      // dueDate.innerText="Due Date: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["dueDate"]);
+      // taskDiv.append(dueDate);
+
+      // const priority=document.createElement("p");
+      // priority.innerText="Priority: " + JSON.stringify(currentLocalStorage["urgentAndImportant"][0]["priority"]);
+      // taskDiv.append(priority);
 
 
 
+    
 
     }
 
